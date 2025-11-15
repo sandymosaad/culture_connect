98267b85961f99de034f27ccbfdcb6e8e3cf809a
Sandy <sandymosaad24@gmail.com>
2025-11-15 17:37:00 +0200

    Implement favorite posts feature and save favorites in localStorage

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 function deletePost(postId) {
     fetch(`/delete/${postId}`, {method: "POST"})
     .then(res => res.json())
@@ -123,4 +125,52 @@ function filterPosts() {
 countryFilter.addEventListener('change', filterPosts);
 categoryFilter.addEventListener('change', filterPosts);
 
+function addPostToFavorite(postId ){
+    if(CURRENT_USER=='None'){
+        const errorContainer = document.createElement('div');
+        errorContainer.classList.add('my-alert' , 'error-alert')
 
+        const errorElement = document.createElement('span');
+        errorElement.textContent = 'You should login first to add favorite posts';
+        errorContainer.appendChild(errorElement);
+        // use [0] to access the first element in HTMLCollection
+        const alertSection = document.getElementsByClassName('section-alert')[0];
+        alertSection.appendChild(errorContainer)
+        setTimeout(() => {
+            errorContainer.style.display = 'none'
+        }, 3000);
+        return
+    }
+    let key = `favorites_${CURRENT_USER}`
+    let icon =  document.getElementById(`favorite-${postId}`)
+    let favorites = JSON.parse(localStorage.getItem(key)) || []
+
+    if (icon.classList.contains('active')){
+        icon.classList.remove("active")
+        const index = favorites.indexOf(postId)
+        if (index > -1){
+            favorites.splice(index, 1);
+        }
+    }else{
+        if(!favorites.includes(postId)){
+        favorites.push(postId)
+        icon.classList.add("active")
+        }
+    }
+
+    localStorage.setItem(key , JSON.stringify(favorites))
+}
+function loadFavorites(){
+    let key = `favorites_${CURRENT_USER}`
+    let favorites = JSON.parse(localStorage.getItem(key)) || []
+    if(favorites.length>0){
+        favorites.forEach(postId => {
+        let icon = document.getElementById(`favorite-${postId}`);
+        if (icon) {
+            icon.classList.add("active");
+        }
+    });
+    }
+
+}
+document.addEventListener("DOMContentLoaded", loadFavorites);
